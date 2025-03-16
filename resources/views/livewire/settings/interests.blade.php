a5edd719c4925daabc41158146981bfdba09d029
Rezo <rezabagheri@gmail.com>
2025-03-16 16:01:18 +0400

    Update Interested

--- resources/views/livewire/settings/interests.blade.php
+++ resources/views/livewire/settings/interests.blade.php
@@ -14,12 +14,7 @@ new class extends Component {
 
     public function mount(): void
     {
-        $this->questionsByPage = Question::with('options')
-            ->orderBy('page')
-            ->orderBy('order_in_page')
-            ->get()
-            ->groupBy('page')
-            ->toArray();
+        $this->questionsByPage = Question::with('options')->orderBy('page')->orderBy('order_in_page')->get()->groupBy('page')->toArray();
 
         $userAnswers = UserAnswer::where('user_id', Auth::id())->get()->pluck('answer', 'question_id')->toArray();
         foreach ($userAnswers as $questionId => $answer) {
@@ -79,11 +74,15 @@ new class extends Component {
         try {
             $user = Auth::user();
 
-            $changedAnswers = array_filter($this->answers, function ($answer, $questionId) {
-                $original = $this->originalAnswers[$questionId] ?? null;
-                $question = Question::find($questionId);
-                return (!$question || $question->is_editable || is_null($original)) && json_encode($answer) !== json_encode($original);
-            }, ARRAY_FILTER_USE_BOTH);
+            $changedAnswers = array_filter(
+                $this->answers,
+                function ($answer, $questionId) {
+                    $original = $this->originalAnswers[$questionId] ?? null;
+                    $question = Question::find($questionId);
+                    return (!$question || $question->is_editable || is_null($original)) && json_encode($answer) !== json_encode($original);
+                },
+                ARRAY_FILTER_USE_BOTH,
+            );
 
             foreach ($changedAnswers as $questionId => $answer) {
                 $question = Question::find($questionId);
@@ -127,24 +126,27 @@ new class extends Component {
                         'answer' => $this->answers[$questionId],
                         'allowed_options' => $options,
                     ]);
-                    $this->validate([
-                        "answers.$questionId" => $rules,
-                        "answers.$questionId.*" => ['in:' . implode(',', $options)],
-                    ], $messages);
+                    $this->validate(
+                        [
+                            "answers.$questionId" => $rules,
+                            "answers.$questionId.*" => ['in:' . implode(',', $options)],
+                        ],
+                        $messages,
+                    );
                 } else {
                     \Log::info("Validating question: {$questionId}", [
                         'answer' => $this->answers[$questionId],
                         'allowed_options' => $question->answer_type === 'single' ? $options : null,
                     ]);
-                    $this->validate([
-                        "answers.$questionId" => $rules,
-                    ], $messages);
+                    $this->validate(
+                        [
+                            "answers.$questionId" => $rules,
+                        ],
+                        $messages,
+                    );
                 }
 
-                UserAnswer::updateOrCreate(
-                    ['user_id' => $user->id, 'question_id' => $questionId],
-                    ['answer' => json_encode($answer)]
-                );
+                UserAnswer::updateOrCreate(['user_id' => $user->id, 'question_id' => $questionId], ['answer' => json_encode($answer)]);
             }
 
             $this->dispatch('interests-updated');
@@ -163,90 +165,106 @@ new class extends Component {
     }
 }; ?>
 
-<div class="card card-primary card-outline card-hover-effect">
-    <div class="card-header">
-        <h5 class="card-title">
-            {{ __('Page :current of :total', ['current' => $currentPage, 'total' => count($questionsByPage)]) }}
-        </h5>
-    </div>
-    <div class="card-body">
-        <form wire:submit.prevent="saveAnswers" class="form-horizontal">
-            @foreach ($questionsByPage[$this->currentPage] ?? [] as $question)
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">{{ $question['question'] }}</label>
-                    <div class="col-sm-9">
-                        @php
-                            $isReadonly = $this->isReadonly($question['id']);
-                        @endphp
-                        @switch($question['answer_type'])
-                            @case('string')
-                                <input wire:model.defer="tempAnswers.{{ $question['id'] }}" type="text" class="form-control" @if($isReadonly) readonly @endif />
+<x-settings.layout heading="Interested" subheading="">
+    <div class="card card-primary card-outline card-hover-effect">
+        <div class="card-header">
+            <h5 class="card-title">
+                {{ __('Page :current of :total', ['current' => $currentPage, 'total' => count($questionsByPage)]) }}
+            </h5>
+        </div>
+        <div class="card-body">
+            <form wire:submit.prevent="saveAnswers" class="form-horizontal">
+                @foreach ($questionsByPage[$this->currentPage] ?? [] as $question)
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">{{ $question['question'] }}</label>
+                        <div class="col-sm-9">
+                            @php
+                                $isReadonly = $this->isReadonly($question['id']);
+                            @endphp
+                            @switch($question['answer_type'])
+                                @case('string')
+                                    <input wire:model.defer="tempAnswers.{{ $question['id'] }}" type="text"
+                                        class="form-control" @if ($isReadonly) readonly @endif />
                                 @break
-                            @case('number')
-                                <input wire:model.defer="tempAnswers.{{ $question['id'] }}" type="number" class="form-control" @if($isReadonly) readonly @endif />
+
+                                @case('number')
+                                    <input wire:model.defer="tempAnswers.{{ $question['id'] }}" type="number"
+                                        class="form-control" @if ($isReadonly) readonly @endif />
                                 @break
-                            @case('boolean')
-                                <select wire:change="updateAnswer('{{ $question['id'] }}', $event.target.value)" class="form-control" @if($isReadonly) disabled @endif>
-                                    <option value="">{{ __('Select') }}</option>
-                                    <option value="1" {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === '1' ? 'selected' : '' }}>{{ __('Yes') }}</option>
-                                    <option value="0" {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === '0' ? 'selected' : '' }}>{{ __('No') }}</option>
-                                </select>
+
+                                @case('boolean')
+                                    <select wire:change="updateAnswer('{{ $question['id'] }}', $event.target.value)"
+                                        class="form-control" @if ($isReadonly) disabled @endif>
+                                        <option value="">{{ __('Select') }}</option>
+                                        <option value="1"
+                                            {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === '1' ? 'selected' : '' }}>
+                                            {{ __('Yes') }}</option>
+                                        <option value="0"
+                                            {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === '0' ? 'selected' : '' }}>
+                                            {{ __('No') }}</option>
+                                    </select>
                                 @break
-                            @case('single')
-                                <select wire:change="updateAnswer('{{ $question['id'] }}', $event.target.value)" class="form-control" @if($isReadonly) disabled @endif>
-                                    <option value="">{{ __('Select') }}</option>
-                                    @foreach ($question['options'] as $option)
-                                        <option value="{{ $option['option_value'] }}" {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === $option['option_value'] ? 'selected' : '' }}>
-                                            {{ $option['option_value'] }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                <small>Selected: {{ $tempAnswers[$question['id']] ?? 'None' }}</small>
+
+                                @case('single')
+                                    <select wire:change="updateAnswer('{{ $question['id'] }}', $event.target.value)"
+                                        class="form-control" @if ($isReadonly) disabled @endif>
+                                        <option value="">{{ __('Select') }}</option>
+                                        @foreach ($question['options'] as $option)
+                                            <option value="{{ $option['option_value'] }}"
+                                                {{ isset($tempAnswers[$question['id']]) && $tempAnswers[$question['id']] === $option['option_value'] ? 'selected' : '' }}>
+                                                {{ $option['option_value'] }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    <small>Selected: {{ $tempAnswers[$question['id']] ?? 'None' }}</small>
                                 @break
-                            @case('multiple')
-                                <select class="form-control" multiple @if($isReadonly) disabled @endif onchange="updateMultipleAnswer('{{ $question['id'] }}', this)">
-                                    @foreach ($question['options'] as $option)
-                                        <option value="{{ $option['option_value'] }}" {{ isset($tempAnswers[$question['id']]) && is_array($tempAnswers[$question['id']]) && in_array($option['option_value'], $tempAnswers[$question['id']]) ? 'selected' : '' }}>
-                                            {{ $option['option_value'] }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                <small>Selected: {{ json_encode($tempAnswers[$question['id']] ?? []) }}</small>
+
+                                @case('multiple')
+                                    <select class="form-control" multiple @if ($isReadonly) disabled @endif
+                                        onchange="updateMultipleAnswer('{{ $question['id'] }}', this)">
+                                        @foreach ($question['options'] as $option)
+                                            <option value="{{ $option['option_value'] }}"
+                                                {{ isset($tempAnswers[$question['id']]) && is_array($tempAnswers[$question['id']]) && in_array($option['option_value'], $tempAnswers[$question['id']]) ? 'selected' : '' }}>
+                                                {{ $option['option_value'] }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    <small>Selected: {{ json_encode($tempAnswers[$question['id']] ?? []) }}</small>
                                 @break
-                        @endswitch
-                        @if (!$question['is_editable'])
-                            <small class="text-warning d-block mt-1">
-                                {{ __('This question can only be answered once and cannot be edited later.') }}
-                            </small>
+                            @endswitch
+                            @if (!$question['is_editable'])
+                                <small class="text-warning d-block mt-1">
+                                    {{ __('This question can only be answered once and cannot be edited later.') }}
+                                </small>
+                            @endif
+                        </div>
+                    </div>
+                @endforeach
+
+                <div class="row">
+                    <div class="col-sm-9 offset-sm-3 d-flex gap-2">
+                        @if ($currentPage > 1)
+                            <button type="button" wire:click="previousPage" class="btn btn-secondary">
+                                <i class="bi bi-arrow-left me-1"></i> {{ __('Previous') }}
+                            </button>
                         @endif
+                        @if ($currentPage < count($questionsByPage))
+                            <button type="button" wire:click="nextPage" class="btn btn-primary">
+                                {{ __('Next') }} <i class="bi bi-arrow-right ms-1"></i>
+                            </button>
+                        @endif
+                        <button type="submit" class="btn btn-primary">
+                            <i class="bi bi-save me-1"></i> {{ __('Save') }}
+                        </button>
                     </div>
                 </div>
-            @endforeach
-
-            <div class="row">
-                <div class="col-sm-9 offset-sm-3 d-flex gap-2">
-                    @if ($currentPage > 1)
-                        <button type="button" wire:click="previousPage" class="btn btn-secondary">
-                            <i class="bi bi-arrow-left me-1"></i> {{ __('Previous') }}
-                        </button>
-                    @endif
-                    @if ($currentPage < count($questionsByPage))
-                        <button type="button" wire:click="nextPage" class="btn btn-primary">
-                            {{ __('Next') }} <i class="bi bi-arrow-right ms-1"></i>
-                        </button>
-                    @endif
-                    <button type="submit" class="btn btn-primary">
-                        <i class="bi bi-save me-1"></i> {{ __('Save') }}
-                    </button>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
-
+</x-settings.layout>
 <script>
-function updateMultipleAnswer(questionId, selectElement) {
-    const selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value);
-    @this.call('updateAnswer', questionId, selectedOptions);
-}
+    function updateMultipleAnswer(questionId, selectElement) {
+        const selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value);
+        @this.call('updateAnswer', questionId, selectedOptions);
+    }
 </script>
